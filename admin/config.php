3a4d5623fbc4745506368b60d4af77319648af0d
gswork <g.stoenchev@stl.bg>
2024-07-25 17:25:28 +0300

    init

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/');
+
+// DIR
+define('DIR_APPLICATION', '/home/jorro/stl/speedy_v3/admin/');
+define('DIR_SYSTEM', '/home/jorro/stl/speedy_v3/system/');
+define('DIR_IMAGE', '/home/jorro/stl/speedy_v3/image/');
+define('DIR_STORAGE', '/home/jorro/stl/speedy_v3/storagee/');
+define('DIR_CATALOG', '/home/jorro/stl/speedy_v3/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'stl');
+define('DB_PASSWORD', 'newPass');
+define('DB_DATABASE', 'speedy_v3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
